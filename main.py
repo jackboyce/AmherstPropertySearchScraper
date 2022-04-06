dea2e8630e856d6a6b45fbab679f74b60fb419a7
Jack Boyce <jackboyce3131@gmail.com>
2022-04-06 14:31:50 -0400

    Threaded search by street

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException,
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
+import queue
+from threading import Thread
 import openpyxl
 import PyPDF2 as pypdf
 from lxml.etree import tostring
+from urllib3.exceptions import MaxRetryError
+
 
 def wait_for_element_removal(element, timeout_limit):
     element_removed = False
@@ -40,19 +44,10 @@ def inner_html_to_value_list(inner_html):
             ret_prop_list.append([address, owner, description, prop_card])
     return ret_prop_list
 
-url = "https://gis.amherstma.gov/apps/assessment/PropertySearchInlineRpEmbed.aspx"
+# url = "https://gis.amherstma.gov/apps/assessment/PropertySearchInlineRpEmbed.aspx"
 file = "rentalpropsstreet"
 fields = ['Address', 'Owner', 'Description', 'Prop_Card']
 prop_list = []
-table_present = EC.presence_of_element_located((By.ID, 'ctl00_ContentPlaceHolderPanel_GridView1'))
-
-DRIVER_PATH = 'chromedriver.exe'
-service = Service(DRIVER_PATH)
-options = webdriver.ChromeOptions()
-options.headless = True
-timeout = 5
-
-driver = webdriver.Chrome(service=service, options=options)
 
 # Load streets.txt file which contains all of the desired streets
 streets = []
@@ -64,50 +59,117 @@ with open('streets.txt') as f:
 if not streets:
     streets = ['%']
 
+# streets = ['FEARING ST', 'AMITY ST', 'NUTTING AVE']
+
+class Worker(Thread):
+    def __init__(self, request_queue):
+        Thread.__init__(self)
+        self.queue = request_queue
+
+        self.results = []
+
+    def run(self):
+        while True:
+            street_from_queue = self.queue.get()
+            if street_from_queue == "stop":
+                break
+            elif street_from_queue == "":
+                continue
+
+            url = "https://gis.amherstma.gov/apps/assessment/PropertySearchInlineRpEmbed.aspx"
+            table_present = EC.presence_of_element_located((By.ID, 'ctl00_ContentPlaceHolderPanel_GridView1'))
+
+            DRIVER_PATH = 'chromedriver.exe'
+            service = Service(DRIVER_PATH)
+            options = webdriver.ChromeOptions()
+            options.headless = True
+            timeout = 5
+
+            driver = webdriver.Chrome(service=service, options=options)
+            try:
+                driver.get(url)
+            except Exception as e:
+               print(e)
+               driver.quit()
+               self.queue.task_done()
+
+
+            # Select street from streets list
+            selenium.webdriver.support.select.Select(
+                driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_ddlStreet')).select_by_value(street_from_queue)
+            print("Selecting " + street_from_queue)
+
+            # Select the rental checkbox and then press submit
+            driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_chkHasRP').click()
+            driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_btnSubmit').click()
+
+            # Wait for the properties to load in the first table
+            try:
+                WebDriverWait(driver, timeout).until(table_present)
+            except TimeoutException:
+                print("Timeout")
+
+            table_html = driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_GridView1').get_attribute(
+                "innerHTML")
+
+            # Process the table html and add value list to final list list
+            self.results.append(inner_html_to_value_list(table_html))
+
+            # Load new pages and add data to final list list. Contains final page exception break
+            for page_num in range(2, 160):
+                print("Loading page " + str(page_num))
+
+                table = driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_GridView1')
+
+                driver.execute_script(
+                    "__doPostBack('ctl00$ContentPlaceHolderPanel$GridView1','Page$" + str(page_num) + "')")
+                if "Invalid postback or callback argument" in str(driver.title):
+                    print("End of pages")
+                    break
+
+                try:
+                    wait_for_element_removal(table, timeout)
+                    WebDriverWait(driver, timeout).until(table_present)
+                except TimeoutException:
+                    print("Timeout on new pageload " + str(page_num))
+
+                table_html = driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_GridView1').get_attribute(
+                    "innerHTML")
+                self.results.append(inner_html_to_value_list(table_html))
+                time.sleep(0)
+
+            driver.quit()
+            self.queue.task_done()
+
+
+q = queue.Queue()
+num_workers = 11
+
+# Add unchecked proxy strings to the queue
 for street in streets:
+    q.put(street)
 
-    # Load new page of website for each street loop
-    driver.get(url)
+# Add the break case to the queue to end the workers
+for _ in range(num_workers):
+    q.put("stop")
 
-    # Select street from streets list
-    selenium.webdriver.support.select.Select(driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_ddlStreet')).select_by_value(street)
-    print("Selecting " + street)
+# Run the workers and add them to a list of workers
+workers = []
+for _ in range(num_workers):
+    worker = Worker(q)
+    worker.start()
+    workers.append(worker)
 
-    # Select the rental checkbox and then press submit
-    driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_chkHasRP').click()
-    driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_btnSubmit').click()
+# Wait for the workers to all be finished before proceeding
+for worker in workers:
+    worker.join()
 
-    # Wait for the properties to load in the first table
-    try:
-        WebDriverWait(driver, timeout).until(table_present)
-    except TimeoutException:
-        print("Timeout")
+# Add the checked results of workers to the checked_proxy_list
+for worker in workers:
+    for results in worker.results:
+        prop_list += results
 
-    table_html = driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_GridView1').get_attribute("innerHTML")
 
-    # Process the table html and add value list to final list list
-    prop_list += inner_html_to_value_list(table_html)
-
-    # Load new pages and add data to final list list. Contains final page exception break
-    for page_num in range(2, 160):
-        print("Loading page " + str(page_num))
-        table = driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_GridView1')
-
-        driver.execute_script("__doPostBack('ctl00$ContentPlaceHolderPanel$GridView1','Page$" + str(page_num) + "')")
-        if "Invalid postback or callback argument" in str(driver.title):
-            print("End of pages")
-            break
-
-        try:
-            wait_for_element_removal(table, timeout)
-            WebDriverWait(driver, timeout).until(table_present)
-        except TimeoutException:
-            print("Timeout on new pageload " + str(page_num))
-
-        table_html = driver.find_element(By.ID, 'ctl00_ContentPlaceHolderPanel_GridView1').get_attribute("innerHTML")
-        prop_list += inner_html_to_value_list(table_html)
-
-driver.quit()
 
 # Write results to csv file
 with open(file + '.csv', "w") as csvfile:
